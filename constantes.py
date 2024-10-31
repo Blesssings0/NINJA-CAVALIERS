3514748f8fe4e1b41c07e5eb9fae61ee2d83c323
Yuzu <Yuzu_0204@outlook.com>
2024-10-30 23:30:00 -0400

    fix: :bug: Resolve Error while trying to use 1600 tiles as a background of the game

--- constantes.py
+++ constantes.py
@@ -1,7 +1,7 @@
 import pygame
 
 WIDTH = 800
-HEIGHT =600
+HEIGHT = 600
 
 
 Ancho_PERSONAJE = 50
@@ -11,8 +11,8 @@ Color_bg = "white"
 Rojo = (203, 50, 52)
 
 
-Velocidad_Personaje =5
-Posicion_initial = (0,0)
+Velocidad_Personaje = 5
+Posicion_initial = (0, 0)
 Scala_personaje = 0.8
 
 
@@ -21,11 +21,10 @@ Alto_ENEMIGO = 10
 Ancho_ENEMIGO = 50
 
 
-
 # Colores para las líneas
 COLOR_LINEA = (255, 255, 255)  # Color blanco para las líneas de los tiles
 
-Tiles_Type = 1600
-Tile_Size = 50# Tamaño del tile (puede ser ajustado según el diseño)
-FILAS = 50
-COLUMNAS = 50
+Tiles_Type = 1
+Tile_Size = 50  # Tamaño del tile (puede ser ajustado según el diseño)
+FILAS = 1
+COLUMNAS = 1
